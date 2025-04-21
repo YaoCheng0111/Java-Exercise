26a59a840427a103c66faa26bc4cb55c19596ee3
Ryderfeng <105143043+Ryderfeng@users.noreply.github.com>
2025-04-22 00:51:57 +0800

    feat: second version-print the second member name

--- FirstClass4Team.java
+++ FirstClass4Team.java
@@ -1,6 +1,9 @@
+
 public class FirstClass4Team {
+
     public static void main(String[] args) {
         System.out.println("這是用來練習GitHub協作的程式!");
         System.out.println("\"學號：\"01257104\"\n姓名：\"吳燿丞\"");
+        System.out.println("\"學號：\"01257101\"\n姓名：\"馮均讀\"");
     }
 }
