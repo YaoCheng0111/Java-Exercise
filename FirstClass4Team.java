c0a3119b972b543caf1e89bcbd38923470cb771c
HankWang <xihongw103@gmail.com>
2025-04-22 12:32:07 +0800

    feat: third version-print the third member name

--- FirstClass4Team.java
+++ FirstClass4Team.java
@@ -5,5 +5,6 @@ public class FirstClass4Team {
         System.out.println("這是用來練習GitHub協作的程式!");
         System.out.println("\"學號：\"01257104\"\n姓名：\"吳燿丞\"");
         System.out.println("\"學號：\"01257101\"\n姓名：\"馮均讀\"");
+        System.out.println("\"學號：\"01257149\"\n姓名：\"王希宏\"");
     }
 }
